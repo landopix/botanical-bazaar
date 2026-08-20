bfb43b9b6493034376afc3055b52f1cddb4c5ce0
landopix <landon.pixley@gmail.com>
2026-08-20 09:54:41 -0400

    fix(admin): use dynamic import with ssr: false for NextStudio

--- pages/admin/[[...index]].js
+++ pages/admin/[[...index]].js
@@ -1,6 +1,11 @@
 import Head from 'next/head';
-import { NextStudio } from 'next-sanity/studio';
+import dynamic from 'next/dynamic';
 import config from '../../sanity.config';
+
+const NextStudio = dynamic(
+  () => import('next-sanity/studio').then((mod) => mod.NextStudio),
+  { ssr: false }
+);
 
 export default function AdminPage() {
   return (
